232c79085b94b873dc4f1ebbc0773e8dd5326aa0
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2025-03-28 20:14:08 -0300

    payment successful email styles and text

--- onepage-ecnu-team/app/emails/payment_successful_template.tsx
+++ onepage-ecnu-team/app/emails/payment_successful_template.tsx
@@ -6,6 +6,8 @@ import {
   Html,
   Preview,
   Text,
+  Hr,
+  Img,
 } from "@react-email/components";
 
 interface PaymentSuccessfulEmailType {
@@ -15,13 +17,22 @@ interface PaymentSuccessfulEmailType {
 
 const styles = {
   body: { backgroundColor: "#f6f6f6", padding: "20px" },
-  container: {
+  header: {
+    backgroundColor: "#111",
+  },
+  logo: {
+    margin: "10px auto",
+    width: "180px",
+  },
+  footerImage: {},
+  main: {
     backgroundColor: "#ffffff",
     padding: "20px",
     borderRadius: "5px",
   },
   text: { fontSize: "16px", color: "#333" },
-  footer: { fontSize: "14px", color: "#777", marginTop: "20px" },
+  firm: { fontSize: "14px", color: "#333", marginTop: "20px" },
+  footer: { backgroundColor: "#ffffff", width: "100%" },
 };
 export const PaymentSuccessfulTemplate = ({
   name,
@@ -30,17 +41,35 @@ export const PaymentSuccessfulTemplate = ({
   return (
     <Html>
       <Head />
-      <Preview>Mensaje para {name}</Preview>{" "}
+      <Preview>
+        ¡Bienvenido {name} a tu {plan}!
+      </Preview>{" "}
       {/* Vista previa en clientes de correo */}
       <Body style={styles.body}>
-        <Container style={styles.container}>
-          <Text style={styles.text}>Hola {name}!</Text>
-          <Text style={styles.text}>Tu pago ha sido aprobado!</Text>
-          <Text style={styles.text}>Has adquirido el plan {plan}</Text>
-          <Text style={styles.footer}>
-            —<br />
-            Equipo de ECNU
+        <Container style={styles.header}>
+          <Img src="/images/new-logo.png" alt="ECNU Logo" style={styles.logo} />
+        </Container>
+        <Container style={styles.main}>
+          <Text style={styles.text}>¡Bienvenido {name}!</Text>
+          <Text style={styles.text}>
+            El pago de tu <strong>{plan}</strong> fue aprobado con éxito! El
+            acceso es por <strong>90 días</strong> antes de la primera
+            renovación.
           </Text>
+          <Text style={styles.text}>
+            A la brevedad me pondré en contacto contigo para coordinar nuestra
+            primera videollamada sobre tus objetivos.
+          </Text>
+          <Text style={styles.text}>¡Muchas gracias!</Text>
+          <Hr />
+          <Text style={styles.firm}>Equipo de ECNU</Text>
+        </Container>
+        <Container style={styles.footer}>
+          <Img
+            src="/images/emails/payment-successful/footer-image.jpg"
+            alt="ECNU Logo"
+            style={styles.footerImage}
+          />
         </Container>
       </Body>
     </Html>
@@ -49,7 +78,7 @@ export const PaymentSuccessfulTemplate = ({
 
 PaymentSuccessfulTemplate.PreviewProps = {
   name: "John Doe",
-  plan: "Plan Plus",
+  plan: "Plan Plus - Gym Virtual",
 } as PaymentSuccessfulEmailType;
 
 export default PaymentSuccessfulTemplate;
