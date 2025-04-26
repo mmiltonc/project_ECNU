39d71cb41a9a0c388884b7ee107b577be8baf0c7
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2025-04-26 14:35:26 -0300

    undo home added - undo img to image

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,10 @@ import About from "@/components/about";
 import Motivation from "@/components/motivation";
 import Faq from "@/components/faq";
 import Contact from "@/components/contact";
-import Home from "@/components/home";
 
-export default function Landing() {
+export default function Home() {
   return (
     <ModalProvider>
-      <Home />
       <Header />
       <Introduction />
       <Programs />
